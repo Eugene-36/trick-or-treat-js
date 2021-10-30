88e4a01c65416379969730a5c19c4f643ccd44f5
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2021-10-30 20:59:46 +0300

    pagination, scroll

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,9 @@
-//**================ SHOW MENU================ //
+//*================ SHOW MENU================ //
 const navMenu = document.getElementById('nav-menu'),
   navToggle = document.getElementById('nav-toggle'),
   navClose = document.getElementById('nav-close');
 
-//** */ ===== MENU SHOW ======================
+//* ===== MENU SHOW ======================
 // Validate if constant exists
 if (navToggle) {
   navToggle.addEventListener('click', () => {
@@ -11,7 +11,7 @@ if (navToggle) {
   });
 }
 
-//** */======MENU HIDDEN ======
+//*======MENU HIDDEN ======
 // Validate if constant exists
 if (navClose) {
   navClose.addEventListener('click', () => {
@@ -20,6 +20,7 @@ if (navClose) {
 }
 
 //**===================REMOVE MENU MOBILE========================= */
+
 const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction() {
@@ -29,3 +30,25 @@ function linkAction() {
   navMenu.classList.remove('show-menu');
 }
 navLink.forEach((n) => n.addEventListener('click', linkAction));
+
+//* ================HOME SWIPER ==========================
+let homeSwiper = new Swiper('.home-swiper', {
+  spaceBetween: 30,
+  loop: 'true',
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+//*=========================== CHANGE BACKGROUND HEADER========================
+
+function scrollHeader() {
+  const header = document.getElementById('header');
+
+  //When the scroll is greater than 100 viewport height , add
+  if (this.scrollY >= 50) header.classList.add('scroll-header');
+  else header.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader);
